f8fec19e699ad06ef5400dd2d7d72afda89c2951
LeCHAUVEaLaCape <brian.peyre@gmail.com>
2022-01-22 21:56:34 +0100

    affiche les publications dans le bonne ordre: index

--- actions/articles/allAuthorsPosts.php
+++ actions/articles/allAuthorsPosts.php
@@ -1,5 +1,5 @@
 <?php
 require('actions/database.php');
 
-$getAllAuthorsArticles = $bdd->prepare('SELECT id,title,description,content,date,author_username,author_id FROM articles');
+$getAllAuthorsArticles = $bdd->prepare('SELECT id,title,description,content,date,author_username,author_id FROM articles ORDER BY id DESC');
 $getAllAuthorsArticles->execute();
